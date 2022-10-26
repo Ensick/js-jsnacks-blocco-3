c29f281c9fe9354a895d152d789f667fdf587f06
Ensick <fede.censi96@gmail.com>
2022-10-26 16:42:43 +0200

    Snack-4 completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,3 +92,26 @@ while(x < numeroUtente){
 }
 
  */
+
+/* Snack-4 */
+/* 
+let listaNomi = ["Andrea", "Nicolo", "Leonardo"];
+let i=0;
+let soldatino = false;
+
+let nome = prompt("Inserisci il nome");
+
+while(i < listaNomi.length)
+{
+    if(nome != listaNomi[i])
+    {
+        soldatino = false;
+    }
+    else
+    {
+        soldatino = true;
+        break;
+    }
+
+    i++;
+} */
